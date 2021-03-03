87808c09b72589b89ded1b21a2cddc5b0afabc4a
finalch <finalch@163.com>
2021-03-03 11:04:25 +0800

    local test code

--- src/test/java/com/finalch/mybatis/cache/TestCache.java
+++ src/test/java/com/finalch/mybatis/cache/TestCache.java
@@ -1,12 +1,16 @@
-package com.finalch.mybatis;
+package com.finalch.mybatis.cache;
 
+import com.finalch.mybatis.dao.UserMapper;
+import com.finalch.mybatis.po.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.Transaction;
 import org.apache.ibatis.transaction.jdbc.JdbcTransaction;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,10 +26,20 @@ public class TestCache {
 
   @BeforeEach
   public void init() throws IOException, SQLException {
-    InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+    InputStream inputStream = Resources.getResourceAsStream("com/finalch/mybatis/cache/mybatis-config.xml");
     // 实例化sqlSessionFactory   ----  DefaultSqlSessionFactory
     sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
     configuration = sqlSessionFactory.getConfiguration();
     transaction = new JdbcTransaction(configuration.getEnvironment().getDataSource().getConnection());
+  }
+
+  @Test
+  public void localCacheTest() {
+    SqlSession sqlSession = sqlSessionFactory.openSession();
+    UserMapper userMapper = sqlSession.getMapper(UserMapper.class);
+    User user = userMapper.getUserById(1);
+    System.out.println(user == null ? "null" : user.getName());
+    User user1 = userMapper.getUserById(1);
+    System.out.println(user1 == null ? "null" : user1.getName());
   }
 }
